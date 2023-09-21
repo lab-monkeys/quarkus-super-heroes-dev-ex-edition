fcc1cc86982b562052e52f674397fdc9803aa93c
Charro Gruver <null>
2023-09-21 17:09:52 +0000

    update env file

--- dev-init/dev-env.sh
+++ dev-init/dev-env.sh
@@ -1,57 +1,57 @@
 #!/usr/bin/env bash
 
-JAVA_HOME="/etc/alternatives/jre_17"
-UI_HTTP_PORT=8080
-HEROES_PORT=8081
-VILLAINS_PORT=8082
-FIGHTS_PORT=8083
+export UI_HTTP_PORT=8080
+export HEROES_PORT=8081
+export VILLAINS_PORT=8082
+export FIGHTS_PORT=8083
 
 
 function setUiEnv() {
-
+  export DEBUG_PORT=5005
+  export APP_PORT=${UI_HTTP_PORT}
 }
 
 function setHeroesEnv() {
-  DEBUG_PORT=5007
-  APP_PORT=${HEROES_PORT}
-  DB_USER=superman
-  DB_PWD=superman
-  DB_HOST=localhost
-  DB_SCHEMA=heroes_database
-  CORS_ORIGINS="/.*/"
-  CORS_METHODS="GET,POST,DELETE"
+  export DEBUG_PORT=5007
+  export APP_PORT=${HEROES_PORT}
+  export DB_USER=superman
+  export DB_PWD=superman
+  export DB_HOST=localhost
+  export DB_SCHEMA=heroes_database
+  export CORS_ORIGINS="/.*/"
+  export CORS_METHODS="GET,POST,DELETE"
 }
 
 function setVillainsEnv() {
-  DEBUG_PORT=5006
-  APP_PORT=${VILLAINS_PORT}
-  DB_USER=suberbad
-  DB_PWD=suberbad
-  DB_HOST=suberbad
-  DB_SCHEMA=villains_database
-  CORS_ORIGINS="/.*/"
-  CORS_METHODS="GET,POST,DELETE"
+  export DEBUG_PORT=5006
+  export APP_PORT=${VILLAINS_PORT}
+  export DB_USER=suberbad
+  export DB_PWD=suberbad
+  export DB_HOST=suberbad
+  export DB_SCHEMA=villains_database
+  export CORS_ORIGINS="/.*/"
+  export CORS_METHODS="GET,POST,DELETE"
 }
 
 function setFightsEnv() {
-  DEBUG_PORT=5008
-  APP_PORT=${FIGHTS_PORT}
-  HERO_HOST=localhost
-  VILLAIN_HOST=localhost
-  DB_USER=superfight
-  DB_PWD=superfight
-  DB_HOST=localhost
-  DB_SCHEMA=fights
-  CORS_ORIGINS="/.*/"
-  CORS_METHODS="GET,POST,DELETE"
+  export DEBUG_PORT=5008
+  export APP_PORT=${FIGHTS_PORT}
+  export HERO_HOST=localhost
+  export VILLAIN_HOST=localhost
+  export DB_USER=superfight
+  export DB_PWD=superfight
+  export DB_HOST=localhost
+  export DB_SCHEMA=fights
+  export CORS_ORIGINS="/.*/"
+  export CORS_METHODS="GET,POST,DELETE"
 }
 
 function setStatisticsEnv() {
-    
+  export DEBUG_PORT=5009
 }
 
 function startDevMode() {
-  quarkus dev -Dquarkus.analytics.disabled=false -Dmaven.repo.local=/home/user/.m2/repository -Dquarkus.http.host=0.0.0.0 -Dmaven.test.skip -Ddebug=${DEBUG_PORT}
+  quarkus dev -Dquarkus.analytics.disabled=true -Dmaven.repo.local=/home/user/.m2/repository -Dquarkus.http.host=0.0.0.0 -Dmaven.test.skip -Ddebug=${DEBUG_PORT}
 }
 
 for i in "$@"
